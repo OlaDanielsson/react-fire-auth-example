5e1787b1ae1fe577c9618dad3f2e227ebb747165
Ola Danielsson <danielsson.ola@hotmail.com>
2021-04-28 15:23:34 +0200

    Complete firebaseconfig

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,9 +1,10 @@
 export default {
-  apiKey: "AIzaSyDfu53prPBUJXZKMxryn27MCvFP9zuXnGU",
-  authDomain: "sexy-auth.firebaseapp.com",
-  databaseURL: "https://sexy-auth.firebaseio.com",
-  projectId: "sexy-auth",
-  storageBucket: "",
-  messagingSenderId: "219620184506",
-  appId: "1:219620184506:web:333528e32e721d50"
+   //databaseURL: "https://sexy-auth.firebaseio.com",
+   apiKey: "AIzaSyA0QF7jF2aEDIP6HEhkkYUmOyPUA1VnA-s",
+   authDomain: "auth-flutter-test-1.firebaseapp.com",
+   projectId: "auth-flutter-test-1",
+   storageBucket: "auth-flutter-test-1.appspot.com",
+   messagingSenderId: "2081975950",
+   appId: "1:2081975950:web:2215c54a7cb2f3c95da3df",
+   measurementId: "G-5NVJKHMW18"
 };
